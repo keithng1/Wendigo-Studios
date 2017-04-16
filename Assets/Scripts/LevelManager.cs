49bf5ea1aa551e1d0d22ad1ce3d63ffa0773b02a
William Dionio <wdionio@gmail.com>
2017-04-17 04:55:48 +0800

    Made ayos the UI, added quit button in main menu and made ayos the sprite layerings

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void quitLevel(){
+		Debug.Log ("Quitting Level");
+		Application.Quit ();
+	}
+}
